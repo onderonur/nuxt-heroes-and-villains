cab85e16cdefd010b7705448cef4df5c0db0c35a
onderonur <onuronder.onr@gmail.com>
2022-08-12 19:35:58 +0300

    github pages deployment fix

---
+++ prepareGhPages.js
@@ -0,0 +1,13 @@
+const fs = require('fs/promises');
+
+async function prepareGhPages() {
+  // eslint-disable-next-line no-console
+  console.log('GH Pages preparation is started.');
+  await fs.copyFile('./dist/index.html', './dist/404.html');
+  // eslint-disable-next-line no-console
+  console.log('GH Pages preparation is finished.');
+}
+
+prepareGhPages();
+
+module.exports = prepareGhPages;
